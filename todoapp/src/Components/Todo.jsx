bde4c6a984ccdfa720494533748132655807ebb4
SanjayTripathee <9864226384s@gmail.com>
2024-03-27 12:06:19 +0545

    Displaying our todos

--- todoapp/src/Components/Todo.jsx
+++ todoapp/src/Components/Todo.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 export default function Todo() {
-  const [name, setName] = useState("");
+  const [todo, setTodo] = useState(""); //store current value in variable i.e todo
+  const [todos, setTodos] = useState([]); //store entire thing including previous enter data also
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <div>
-      <form>
-        <input type="text" value={name} />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          value={todo}
+        />
+        <button>Add</button>
       </form>
+      {todos.map((item) => (
+        <TodoItem key={item} item={item} />
+      ))}
     </div>
   );
 }
