fc2bab6ad383354d3ae45f4fa1092a0bb730e82c
SanjayTripathee <9864226384s@gmail.com>
2024-03-31 08:46:34 +0545

    Making marked item display and meaningful

--- todoapp/src/Components/Todo.jsx
+++ todoapp/src/Components/Todo.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 import TOdoList from "./TodoList";
 import Form from "./Form";
+import Footer from "./Footer";
 
 export default function Todo() {
   const [todo, setTodo] = useState({ name: "", done: false }); //store current value in variable i.e todo
   const [todos, setTodos] = useState([]); //store entire thing including previous enter data also
-
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length;
   return (
     <div>
       {/* psssing props from parent to child as they required */}
       <Form setTodo={setTodo} todo={todo} todos={todos} setTodos={setTodos} />
       <TOdoList todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
     </div>
   );
 }
